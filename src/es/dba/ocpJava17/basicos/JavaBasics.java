cbd01839a4c8cee562700b77be023910a3864dea
dbarga6 <dbarga6@gmail.com>
2024-10-06 11:46:43 +0200

    Subida desde eclipse

--- src/es/dba/ocpJava17/basicos/JavaBasics.java
+++ src/es/dba/ocpJava17/basicos/JavaBasics.java
@@ -8,14 +8,24 @@ public class JavaBasics {
 		
 	}
 	
+	/*
+	 * Método para realizar pruebas de los distintos datos primitivos
+	 */
 	public void datosPrimitvos() {
 		System.out.println("*** Sección de datos primitivos ***");
 	}
 	
+	/*
+	 * Método para realizar pruebas de los distintos operadores
+	 */
 	public void operadores() {
 		System.out.println("*** Sección de operadores ***");
 	}
 	
+	/*
+	 * Método para realizar las pruebas de estructuras de control 
+	 * if,swith, bucles, etc.. 
+	 */
 	public void estructurasDeControl() {
 		System.out.println("*** Estructuras de Control ***");
 		
