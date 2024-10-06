af40773e94c3fe6378653b280833feddb97a4915
dbarga6 <dbarga6@gmail.com>
2024-10-06 13:21:18 +0200

    Conexión con jdbc básico, sin framework. La conexión se realiza con la base de datos h2

--- src/es/dba/ocpJava17/OCPJava17.java
+++ src/es/dba/ocpJava17/OCPJava17.java
@@ -1,17 +1,24 @@
 package es.dba.ocpJava17;
 
 import es.dba.ocpJava17.basicos.JavaBasics;
+import es.dba.ocpJava17.imp.jdbc.UsersJdbc;
 
 public class OCPJava17 {
 
 	public static void main(String[] args) {
 		System.out.println("Clase principal");
         llamadaClaseA();
+        consultaUsuarios();
 	}
 	
 	private static void llamadaClaseA() {
 		JavaBasics javaBasics = new JavaBasics();
 		javaBasics.datosPrimitvos();
+	}
+	
+	private static void consultaUsuarios() {
+		UsersJdbc userJdbc = new UsersJdbc();
+		userJdbc.ejecutarConsulta();
 	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,7 @@
  * 
  */
 module OCPJava17 {
+	requires java.sql; //  Necesario para utilizar JDBC
+	//requires com.h2database; // no se puede poner la instrucción requires h2 directamente porque el jar de h2 no contiene un module-info. 
+	                         // debido a este motivo se utiliza un módulo automatico. Java genera un nombre automatico basado en el jar. 
 }
